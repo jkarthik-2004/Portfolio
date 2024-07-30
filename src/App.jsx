340c9026fab861f5275e07c757ddc7b432997870
Karthik Jonnalagadda <jkarthik1947@gmail.com>
2024-07-30 12:47:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ function App() {
                 </button>
                  <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                 <a
-                    href="https://drive.google.com/file/d/1UizXbFubtxVCMniY1K3T04f5nUBYIx4Y/view?usp=sharing"
+                    href="https://drive.google.com/file/d/19D635HUZ6250p0K58HDqa1k6SZ4RErC_/view?usp=drive_link"
                     target="_blank"
                   >
                     Resume
