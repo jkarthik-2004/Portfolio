fe14e5ea894a61a3d206bf646cc90d2a45d72f35
Karthik Jonnalagadda <jkarthik1947@gmail.com>
2024-09-03 17:09:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -278,6 +278,74 @@ function App() {
               </div>
 
 
+
+
+ {/* Bricks Estate */}
+
+              <div className="border border-gray-500 rounded-md p-5 flex-1">
+                <img src={Project5} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">Predictive Maintenance</h3>
+                <p className="text-gray-400 text-sm mt-2">
+                 Predictive Maintenance of Turbo Engine using Machine Learning
+                </p>
+                <div className="flex mt-20 justify-evenly">
+                  <a href="https://predictivemaintenance-team10.streamlit.app/" target="_blank">
+                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Live preview <CiGlobe className="inline ml-2 mb-1"/>
+                      </span>
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/jkarthik-2004/Predictive_Maintenance"
+                    target="_blank"
+                  >
+                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Checkout github
+                        <FaGithub className="inline ml-2 mb-1" />
+                      </span>
+                    </button>
+                  </a>
+                </div>
+              </div>
+
+            
+{/* One-Lot */}
+
+
+              <div className="border border-gray-500 rounded-md p-5 flex-1">
+                <img src={Project6} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">One Lot Hero Page</h3>
+                <p className="text-gray-400 text-sm mt-2">
+                  Hero Page using NextJS, TailwindCss
+                </p>
+                <div className="flex mt-20 justify-evenly">
+                  <a href="https://one-lot-assignment.vercel.app/" target="_blank">
+                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Live preview <CiGlobe className="inline ml-2 mb-1"/>
+                      </span>
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/jkarthik-2004/OneLot-Assignment"
+                    target="_blank"
+                  >
+                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Checkout github
+                        <FaGithub className="inline ml-2 mb-1" />
+                      </span>
+                    </button>
+                  </a>
+                </div>
+              </div>
+
+
+
+              
+
             </div>
 
 
@@ -356,69 +424,7 @@ function App() {
 
             {/* </div> */}
 
-            {/* Bricks Estate */}
-
-
-              <div className="border border-gray-500 rounded-md p-5 flex-1">
-                <img src={Project5} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">Predictive Maintenance</h3>
-                <p className="text-gray-400 text-sm mt-2">
-                 Predictive Maintenance of Turbo Engine using Machine Learning
-                </p>
-                <div className="flex mt-20 justify-evenly">
-                  <a href="https://predictivemaintenance-team10.streamlit.app/" target="_blank">
-                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Live preview <CiGlobe className="inline ml-2 mb-1"/>
-                      </span>
-                    </button>
-                  </a>
-                  <a
-                    href="https://github.com/jkarthik-2004/Predictive_Maintenance"
-                    target="_blank"
-                  >
-                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
-                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Checkout github
-                        <FaGithub className="inline ml-2 mb-1" />
-                      </span>
-                    </button>
-                  </a>
-                </div>
-              </div>
-
-            
-{/* One-Lot */}
-
-
-              <div className="border border-gray-500 rounded-md p-5 flex-1">
-                <img src={Project6} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">One Lot Hero Page</h3>
-                <p className="text-gray-400 text-sm mt-2">
-                  Hero Page using NextJS, TailwindCss
-                </p>
-                <div className="flex mt-20 justify-evenly">
-                  <a href="https://one-lot-assignment.vercel.app/" target="_blank">
-                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Live preview <CiGlobe className="inline ml-2 mb-1"/>
-                      </span>
-                    </button>
-                  </a>
-                  <a
-                    href="https://github.com/jkarthik-2004/OneLot-Assignment"
-                    target="_blank"
-                  >
-                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
-                      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Checkout github
-                        <FaGithub className="inline ml-2 mb-1" />
-                      </span>
-                    </button>
-                  </a>
-                </div>
-              </div>
-
+           
 
          
           </div>
